6114366dd0787ffc89ce486a857a439ec2411bdc
Bobby Jackson <bjackson071967@gmail.com>
2025-07-17 14:20:44 -0500

    Update assembly version and related files
    This commit updates the assembly informational version from "1.0.0+e862380c86d9fcdfcbfbdf431b5ead9fbe6bc9fe" to "1.0.0+af0e0ba68e28e79094a48634ef8b2ac4c094d4d9" across multiple projects, including `Jackson.Ideas.Application`, `Jackson.Ideas.Core`, `Jackson.Ideas.Infrastructure`, and `Jackson.Ideas.Shared`.
    
    Additionally, the `sourcelink.json` files have been updated to reflect the new commit hash. Binary files for various DLLs and PDBs have been modified, indicating changes in the compiled outputs. The `AssemblyInfoInputs.cache` files have new hash values, and the `rbcswa.dswa.cache.json` and `rpswa.dswa.cache.json` files have been updated, suggesting improvements in build cache and asset management.
    
    These changes may include bug fixes or feature enhancements across the application.

--- src/Jackson.Ideas.Application/obj/Debug/net9.0/Jackson.Ideas.Application.AssemblyInfo.cs
+++ src/Jackson.Ideas.Application/obj/Debug/net9.0/Jackson.Ideas.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jackson.Ideas.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e862380c86d9fcdfcbfbdf431b5ead9fbe6bc9fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0e0ba68e28e79094a48634ef8b2ac4c094d4d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jackson.Ideas.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jackson.Ideas.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
